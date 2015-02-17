856bf6305e4086fb448b3c95223a0a1a0274c835
Thomas Goettlicher <tgoettlicher@suse.de>
2015-02-17 12:11:53 +0100

    Fix style issue

--- lib/html.rb
+++ lib/html.rb
@@ -35,7 +35,10 @@ class Html
     File.write(File.join(target, "index.html"), template.render(binding))
     File.write(File.join(target, "assets/description.js"),<<-EOT
       function getDescription() {
-        return JSON.parse('#{description.to_hash.to_json.gsub("\\\"", "&quote").gsub("'", "&#39;")}')
+        return JSON.parse('#{description.to_hash.to_json
+          .gsub("\\\"", "&quote")
+          .gsub("'", "&#39;")}'
+        )
       }
       EOT
     )
